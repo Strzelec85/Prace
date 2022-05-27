6bf20ec831ee3ac0d49057c14d3adcc93a87306b
Belfer <dembowski.p@gmail.com>
2022-05-27 10:20:02 +0200

    Aktualizacja

--- cwiczenia/Zaawansowane/Funkcje.py
+++ cwiczenia/Zaawansowane/Funkcje.py
@@ -1,14 +1,22 @@
 import random
-filer = open("obraz.bmp")
+
+'''Kopiowanie plików
+
 def kopiuj():
     filer = open("rts.bmp")
     filew = open(str(random.randint(1, 1000)) + ".bmp", "w")
     filew.write(filer.read())
     filer.close()
     filew.close()
-'''
+
 for x in range(0, 5):
     kopiuj()
 '''
 
-print(filer.read())
+def pot(a ,b):
+    wynik = 1
+    for x in range(0, b):
+        wynik *= a
+    print(wynik)
+
+pot(2,3)
